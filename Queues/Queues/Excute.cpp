0ec2c4e5ddd1785f447bebea640e0fa85397bd2a
Nam's_Desktop <vbnb0123@naver.com>
2023-08-27 15:26:11 +0900

    Queues 실행 코드 추가

---
+++ Queues/Queues/Excute.cpp
@@ -0,0 +1,32 @@
+﻿#include "Queues.h"
+
+typedef ListQueues<int> Queues;
+
+int main()
+{
+	Queues* queues = new Queues();
+	for (int i = 1; i <= 7; i++)
+	{
+		int* index = new int;
+		*index = *(&i);
+		queues->Push_Back(Queues::Create(&index));
+	}
+
+	queues->Print();
+
+	cout << queues->GetSize() << endl << endl;
+
+	ListQueues<int>::Node* node = queues->Pop_Front();
+
+	cout << *node->Data << endl;
+
+	queues->Print();
+
+	cout << queues->GetSize() << endl << endl;
+
+	queues->RemoveAll();
+
+	cout << queues->GetSize() << endl << endl;
+
+	return 0;
+}
